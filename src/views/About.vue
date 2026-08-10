9fb257184467266daa9f1d9cd839a197c5ae1430
Firefly <107394246+FireflyF09@users.noreply.github.com>
2026-08-10 14:32:36 +0800

    更新 About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -78,7 +78,7 @@
                 <svg class="w-5 h-5 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <span class="text-white">nb3502022@outlook.com</span>
+                <span class="text-white">FireflyF09@outlook.com</span>
               </div>
             </div>
           </div>
